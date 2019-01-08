8dea2e77edf15e91130601615870d3f3caf1d69c
zonghuixu <zonghui.xu@coupang.com>
2019-01-08 16:35:09 +0800

    change script

--- src/main/resources/start_up.sh
+++ src/main/resources/start_up.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+echo `pwd`
+
+hadoop fs -cat /user/set_java_env.sh
+
+hadoop jar /root/jenkins_deploy/hadoop/build/libs/hadoop_startup-1.0-SNAPSHOT.jar  /user/test/test.txt /user/test/output
