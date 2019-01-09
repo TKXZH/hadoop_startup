d59ea9e5ae54af8b9a10a11f34013ba50d727ea4
zonghuixu <zonghui.xu@coupang.com>
2019-01-09 15:41:24 +0800

    add log

--- src/main/java/top/xvzonghui/hadoop/fs/FileOperations.java
+++ src/main/java/top/xvzonghui/hadoop/fs/FileOperations.java
@@ -1,5 +1,6 @@
 package top.xvzonghui.hadoop.fs;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -12,15 +13,19 @@ import java.net.URISyntaxException;
 /**
  * @author zonghuixu
  */
+
+@Slf4j
 public class FileOperations {
 	private static final String HDFS_URI_STR = "hdfs://localhost:9000";
 
 	public void cat(String hdfsPath) throws IOException, URISyntaxException{
+		log.info("now start to print content of file: {}", hdfsPath);
 		Configuration config = new Configuration();
 		FileSystem fs = FileSystem.get(new URI(HDFS_URI_STR), config);
 
 		String fileContent = IOUtils.toString(fs.open(new Path(hdfsPath)));
 		System.out.println(fileContent);
+		log.info("now finish printing content of file: {}", hdfsPath);
 	}
 
 	public static void main(String[] args) throws Exception {
